7610a79fe1252724c03619a7eaadeac042fda66e
Yuliya Ilina <flow987876765@gmail.com>
2022-03-22 23:31:47 +0900

    Corrected files Ball and Fox

--- src/main/java/ru/job4j/oop/Ball.java
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -2,7 +2,7 @@ package ru.job4j.oop;
 
 public class Ball {
     public void tryRun(boolean condition) {
-        if (condition == true) {
+        if (condition) {
             System.out.println("Колбок съеден");
         } else {
             System.out.println("Колобок сбежал");
