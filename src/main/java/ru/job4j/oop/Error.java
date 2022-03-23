5bd7bd8753edd1bc0fc6d3f1fff2ad8ccbdc9277
Yuliya Ilina <flow987876765@gmail.com>
2022-03-23 22:31:24 +0900

    Added file Error

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+        active = true;
+        status = 2;
+        message = "Ошибка";
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.printInfo();
+        Error error1 = new Error(false, 10, "Oк");
+        error1. printInfo();
+    }
+
+    public void printInfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+}
