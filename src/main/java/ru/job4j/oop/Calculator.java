9aba87df0f258fc3390f3cd919a541a3e715cf1b
Yuliya Ilina <flow987876765@gmail.com>
2022-03-24 13:25:36 +0900

    Added file Calculator

---
+++ src/main/java/ru/job4j/oop/Calculator.java
@@ -0,0 +1,41 @@
+package ru.job4j.oop;
+
+public class Calculator {
+    private static int x = 5;
+
+    public static int sum(int y) {
+        return x + y;
+    }
+
+    public int multiply(int a) {
+        return x * a;
+    }
+
+    public static int minus(int t) {
+        return t - x;
+    }
+
+    public int divide(int f) {
+        return f / x;
+    }
+
+    public int sumAllOperation() {
+        return (sum(10) + multiply(5) + minus(16) + divide(6));
+    }
+
+    public static void main(String[] args) {
+        int result = sum(10);
+        System.out.println(result);
+        Calculator calculator = new Calculator();
+        int rsl = calculator.multiply(5);
+        System.out.println(rsl);
+        int rsl1 = minus(16);
+        System.out.println(rsl1);
+        Calculator calculator1 = new Calculator();
+        int rsl2 = calculator1.divide(6);
+        System.out.println(rsl2);
+        Calculator calculator2 = new Calculator();
+        int rsl3 = calculator2.sumAllOperation();
+        System.out.println(rsl3);
+    }
+}
