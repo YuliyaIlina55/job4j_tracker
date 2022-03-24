1cd8dc4514810e8307daa8a72e51723c8468a375
Yuliya Ilina <flow987876765@gmail.com>
2022-03-25 00:50:20 +0900

    Added files Point and PointTest

---
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.oop;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+public class PointTest {
+
+    @Test
+    public void when00to20then2() {
+        int expected = 2;
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        Assert.assertEquals(expected, dist, 0.01);
+    }
+}
