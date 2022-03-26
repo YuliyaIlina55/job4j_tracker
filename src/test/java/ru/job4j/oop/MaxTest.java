7a1cb6a920efafa76806f7fd93987e0fc83e8539
Yuliya Ilina <flow987876765@gmail.com>
2022-03-26 14:19:51 +0900

    Added files Max and MaxTest

---
+++ src/test/java/ru/job4j/oop/MaxTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.oop;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+public class MaxTest {
+
+    @Test
+    public void whenMax1To2Then2() {
+        int first = 1;
+        int second = 2;
+        Max max = new Max();
+        int result = max.max(first, second);
+        int expected = 2;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax1To2To3Then3() {
+        int first = 1;
+        int second = 2;
+        int third = 3;
+        Max max = new Max();
+        int result = max.max(first, max.max(second, third));
+        int expected = 3;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax1To4To3To4Then4() {
+        int first = 1;
+        int second = 4;
+        int third = 3;
+        int fourth = 4;
+        Max max = new Max();
+        int result = max.max(first, max.max(second, third), max.max(third, fourth));
+        int expected = 4;
+        Assert.assertEquals(result, expected);
+    }
+}
